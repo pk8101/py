5cfed3c1c85941d63f4ff7cb20bd9e97a02a2a15
pk8101 <pradeepvirat913@gmail.com>
2023-09-26 20:46:04 +0530

    duplicate values count problem solved

--- Basic/main.py
+++ Basic/main.py
@@ -1,25 +1,10 @@
 a=int(input())
 l=[]
-l1=[]
 for i in range(a):
     l.append(int(input()))
 
-d={}
-for j in l:
-    d[j]=d.get(j,0)+1
-for f in d:
-    if d[f]>=2:
-        l1.append(f)
-h=[]
-for b in l1:
-    for x in range(len(l)):
-        if b==l[x]:
-            h.append(x)
-t=len(h)-1
-h1=0
-while(t>=0):
-    h2=h[t]-h[t-1]
-    if h2>h1:
-        h1=h2
-    t=t-2
-print(h1)
+a=set()
+for s in l:
+    a.add(s)
+
+print(len(l)-len(a))
