28e25d3b835d8b022773aed25280c8d2406f2984
pk8101 <pradeepvirat913@gmail.com>
2023-09-26 18:27:49 +0530

    frequency problem in dict solved

--- Basic/main.py
+++ Basic/main.py
@@ -1,14 +1,25 @@
-print('hello world')
-a={1:2,2:4,5:9}
-print(a.keys())
-print(a.values())
-d=a.copy()
-print(d)
-print(d.items())
-#dict are mutable
-f={2:5}
-a.update(f)
-a.pop(5)
-print(a)
-del a[1]
-print(a)
+# print('hello world')
+# a={1:2,2:4,5:9}
+# print(a.keys())
+# print(a.values())
+# d=a.copy()
+# print(d)
+# print(d.items())
+# #dict are mutable
+# f={2:5}
+# a.update(f)
+# a.pop(5)
+# print(a)
+# del a[1]
+# print(a)
+
+a=int(input())
+l=[]
+for i in range(a):
+    e=int(input())
+    l.append(e)
+d={}
+for j in l:
+    d[j]=d.get(j,0)+1
+
+print(max(d.values()))
