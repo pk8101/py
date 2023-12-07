b53e31c57df09ad37eae3ad82ac3a728a743d306
pk8101 <pradeepvirat913@gmail.com>
2023-12-07 16:56:35 +0530

    sum of n numbers using recursion

--- Basic/recursion.py
+++ Basic/recursion.py
@@ -1,8 +1,11 @@
-def fact(n):
-    if n == 0:
+# sum of n natural  numbers using recursio0n
+
+
+def sum(n):
+    if n == 1:
         return 1
-    return n * fact(n - 1)
+    return n + sum(n - 1)
 
 
 a = int(input())
-print(fact(a))
+print(sum(5))
