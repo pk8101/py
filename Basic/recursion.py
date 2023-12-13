ff3117127aee0fce84364f8341526015740e229c
pk8101 <pradeepvirat913@gmail.com>
2023-12-13 14:20:32 +0530

    tower of hanoi

--- Basic/recursion.py
+++ Basic/recursion.py
@@ -1,34 +1,11 @@
-# sortquick sort using recursio0n
-def findpivot(l,si1,ei1):
-    pivotvalue=l[si1]
-    c=0
-    for i in range(si1,ei1+1):  #finding number of elements smaller than pivotvalue
-        if l[i]<pivotvalue:
-            c=c+1
-    pivotindex=si1+c
-    l[si1+c],l[si1]=l[si1],l[si1+c]
-    i=si1
-    j=ei1
-    while i<j:  #comparing right side and left side of elements with pivot value
-        if l[i]<pivotvalue:
-            i=i+1
-        elif l[j]>=pivotvalue:
-            j=j-1
-        else :
-            l[i],l[j]=l[j],l[i]
-            i=i+1
-            j=j-1
-        
-    return pivotindex
+#tower of hanoi using recursion
 
-def quickSort(a,si,ei):
-    if si>=ei:
+def tower(n,a,b,c):
+    if n==1:
+        print("move 1st from",a," to ",c)
         return
-    pivot=findpivot(a,si,ei)
-    quickSort(a,si,pivot-1)
-    quickSort(a,pivot+1,ei)
+    tower(n-1,a,c,b)
+    print("move ",n,"th disk from",a," to ",c)
+    tower(n-1,b,a,c)
 
-
-a4=[98,3,4,1,7,9,2]
-quickSort(a4,0,len(a4)-1)
-print(a4)
+tower(4,'s','h','d')
