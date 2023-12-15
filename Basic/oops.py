4531994d4431223b8eb2ea7ca0797777e804e2ce
pk8101 <pradeepvirat913@gmail.com>
2023-12-15 10:59:06 +0530

    special method __init__ is implemented its act like a constructor

--- Basic/oops.py
+++ Basic/oops.py
@@ -1,7 +1,11 @@
 class pradeep:
+    def __init__(self,age):
+        self.age=age
+        print("hi pradeep ur age",self.age)
+
     @staticmethod
     def hello():
         print("hello pradeep")
 
-s1=pradeep()
+s1=pradeep(23)
 s1.hello()
