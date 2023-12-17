b8ae7622344ec0bdaad249d9393da3ddc98bff9e
pk8101 <pradeepvirat913@gmail.com>
2023-12-17 18:49:06 +0530

    ' object class' inherited and used its inbuilt method' def __str__():' to provide description for a class

--- Basic/oops.py
+++ Basic/oops.py
@@ -1,12 +1,16 @@
-#simple inheritance 
+#inheriting object class
+# object class contains three methods :
+# 1.def __new__():-> this method used to create new object
+# 2.def __init__():-> this method used to initiate new object
+# 3.def __str__():-> this method used to give the description for the class
+class circle (object):
+    def __init__(self,radius):
+        self.radius=radius
+    def __str__(self):
+        return "this is an circle class which takes radius as an argument"
 
-class A:
-    _name=0
-   
-        
-s=A()
-s._name=20
-print(s.__dict__)
+s=circle(20)
+print(s)
 
 
 
