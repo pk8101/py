2406a847b9096f018711583419508126ee42d3c2
pk8101 <pradeepvirat913@gmail.com>
2023-12-17 18:36:09 +0530

    protected variable implemented '_single under score is protected'

--- Basic/oops.py
+++ Basic/oops.py
@@ -1,16 +1,12 @@
 #simple inheritance 
 
 class A:
-    def __init__(self,name):
-        print("hello ",name)
-    def hello(self):
-        print("hello its class a")
-class B (A):
-    def __init__(self, name):
-        super().__init__(name)
-        print("its class b")
+    _name=0
+   
         
-s=B('pradeep')
+s=A()
+s._name=20
+print(s.__dict__)
 
 
 
