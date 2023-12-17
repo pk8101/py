65392a3899007ec131b2d598e075f30fe1356691
pk8101 <pradeepvirat913@gmail.com>
2023-12-17 23:40:16 +0530

    custom exception error handling done using class and Exception class inherited

--- Basic/main.py
+++ Basic/main.py
@@ -1,9 +1,13 @@
-#custome error handling in this we use raise keyword
+#custome error handling using class
+
+class ZeroDenError(Exception):
+    pass
 a=int(input())
 b=int(input())
 try:
+    
+    if b==0:
+        raise ZeroDenError('den must not be zero')
     c=a//b
-    raise ZeroDivisionError
-
-except ZeroDivisionError:
+except ZeroDenError:
     print("zero error")
