acf093e7dcd58270d5c6cc327e115c64ec75a9dd
pk8101 <pradeepvirat913@gmail.com>
2024-01-05 13:02:40 +0530

    implementing queue using linked list

--- Basic/queue.py
+++ Basic/queue.py
@@ -1,24 +1,37 @@
-#implementing queue using array
+#implementing queue using linked list
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
 class Queue:
     def __init__(self):
-        self.__arr=[]
+        self.__head=None
+        self.__tail=None
         self.__count=0
-        self.__front=0
-    def enqueue(self,data):
-        self.__arr.append(data)
+    def enqueue(self,ele):
+        newNode=Node(ele)
+        if self.__head is None:
+            self.__head=newNode
+        else:
+            self.__tail.next=newNode
+        self.__tail=newNode
         self.__count+=1
 
     def dequeue(self):
-        if self.__count==0:
-            return -1
-        ele=self.__arr[self.__front]
-        self.__front+=1
-        self.__count-=1
-        return ele
+        if self.__head is None:
+            print("heyy!! , queue is empty")
+            return
+        else:
+            data1=self.__head.data
+            self.__head=self.__head.next
+            self.__count-=1
+            return data1
     def front(self):
-        if self.__count==0:
-            return -1
-        return self.__arr[self.__front]
+        if self.__head is None:
+            print("hey queue is empty")
+        else:
+            return self.__head.data
     def size(self):
         return self.__count
     def isempty(self):
@@ -35,3 +48,5 @@ q.enqueue(4)
 while q.isempty() is False:
     print(q.front())
     q.dequeue()
+
+q.front()
