eed75a3a33a41f09516b8a63dbdd3029bcf08e96
pk8101 <pradeepvirat913@gmail.com>
2024-01-05 14:34:17 +0530

    inbulit queue implemented

--- Basic/queue.py
+++ Basic/queue.py
@@ -25,3 +25,14 @@ print(stack)
 
 # while not q.empty():
 #     print(q.get())
+
+
+#inbulit queue implemented
+# import queue
+# q=queue.Queue()
+# q.put(1)
+# q.put(2)
+# q.put(3)
+
+# while not q.empty():
+#     print(q.get())
