805fac0d4e65e4735316a0f1fe9f118c67bdff87
pk8101 <pradeepvirat913@gmail.com>
2024-01-06 15:23:02 +0530

    implemented basic binary tree

---
+++ Basic/BinaryTrees.py
@@ -0,0 +1,17 @@
+# Binary contain maximum of two nodes either(0,1,2)nodes
+# start node cosider as root node it contains left ,right reference for the child nodes
+# trees does not contain an cycles
+
+# implementing basic binary tree
+class BinaryTreeNode:
+    def __init__(self,data):
+        self.data=data
+        self.left=None
+        self.right=None
+
+btn1=BinaryTreeNode(1) #root node
+btn2=BinaryTreeNode(2) #left node
+btn3=BinaryTreeNode(3) #right node
+
+btn1.left=btn2
+btn1.right=btn3
